f895f19ce6f3c7635779b3207736d5915d82a0b0
Joshua Ugba <justjosh001@gmail.com>
2019-09-02 12:43:00 +0100

    updates settings to use heroku db

--- myproject/settings.py
+++ myproject/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': 'jugba', 
-        'PASSWORD': '',  
-        'HOST': '127.0.0.1', 
-        'PORT': '5432', 
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
 
     }
 }
